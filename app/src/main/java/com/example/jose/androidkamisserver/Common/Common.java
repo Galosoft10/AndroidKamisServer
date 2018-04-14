b753ac3fadeb896179b371bb46ae9047545bc386
josetrinidad <j_t_g_l@yahoo.com.mx>
2018-04-13 19:58:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jose/androidkamisserver/Common/Common.java
@@ -0,0 +1,13 @@
+package com.example.jose.androidkamisserver.Common;
+
+import com.example.jose.androidkamisserver.Model.User;
+
+public class Common {
+
+    public static User currentUser;
+
+    public static final  int  PICK_IMAGE_REQUEST = 71;
+
+    public static final String UPDATE = "Update";
+    public static final String DELETE = "Delete";
+}
---
+++ app/src/main/java/com/example/jose/androidkamisserver/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,43 @@
+package com.example.jose.androidkamisserver.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jose.androidkamisserver.Common.Common;
+import com.example.jose.androidkamisserver.Interface.ItemClickListener;
+import com.example.jose.androidkamisserver.R;
+
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener {
+
+    public TextView txtMenuName;
+    public ImageView imageView;
+    private ItemClickListener itemClickListener;
+
+    public MenuViewHolder(View itemView) {
+        super(itemView);
+
+        txtMenuName = itemView.findViewById(R.id.menu_name);
+        imageView = itemView.findViewById(R.id.menu_image);
+        itemView.setOnClickListener(this);
+        itemView.setOnCreateContextMenuListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v, getAdapterPosition(), false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Select an action");
+        menu.add(0,0,getAdapterPosition(), Common.UPDATE);
+        menu.add(0,1, getAdapterPosition(), Common.DELETE);
+    }
+}
